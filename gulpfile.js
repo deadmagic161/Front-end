c1c2f1e977aa2c43339cb0aaccb8bb2c812bd09f
annaVyacheslavovna <anya.kuznetsova19@gmail.com>
2016-10-15 19:17:00 +0700

    new gulpfile without comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 var	concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var browserSync = require('browser-sync').create();
 var clean = require('gulp-clean');
 var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
@@ -11,13 +8,10 @@ var autoprefixer = require('gulp-autoprefixer');
 gulp.task('default', ['clean'], function() {
 	gulp.run('develop');
 }); 
+gulp.task('develop', ['css', 'watch', 'browser-sync', 'assets']); 
 
-gulp.task('develop', ['css', 'useref', 'scripts', 'watch', 'browser-sync', 'assets']);  
-	
-
-//Объединение css файло//
 gulp.task('css', function () {
-	gulp.src('styles/*.css')
+	return gulp.src('styles/*.css')
 		.pipe(concat('styles.css'))
 		.pipe(cssnano())
 		.pipe(autoprefixer({
@@ -25,22 +19,7 @@ gulp.task('css', function () {
 		}))
 		.pipe(gulp.dest('build/'));
 });
-//Объединение js файлов в один
- gulp.task('useref', function() {
-	return gulp.src('index.html')
-		.pipe(useref()) 
-		.pipe(gulp.dest('build/'))
-		.on('end', function() { 
-		gulp.run('scripts');
-		});
-});
-//Минификация скриптов//
- gulp.task('scripts', function() {
-	gulp.src('build/*.js')
-		.pipe(uglify()) 
-		.pipe(gulp.dest('build/'));
-});
-//Запуск сервера.
+
 gulp.task('browser-sync', function() {
 	return browserSync.init({
 		server: {
@@ -48,19 +27,17 @@ gulp.task('browser-sync', function() {
 		}
 	});
 });
-// Установление слежки
+
 gulp.task('watch', function() {
 	gulp.watch('*.css', ['css']);
 	gulp.watch('js/*.js', ['scripts']);
-
 });
 
-//Задача для удаление папки build.
 gulp.task('clean', function() {
 	return gulp.src('build/')
 		.pipe(clean());
 })
-//Перемещение локальных файлов в папку build
+
 gulp.task('assets', function() {
 	return gulp.src('**/*.*')
 		.pipe(gulp.dest('build/'));
