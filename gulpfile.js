c71d3e3cde0459d90c7a8a3a9d67bdae3e9ffe57
deadmagic161 <fernando093@yandex.ru>
2016-10-21 19:41:16 +0700

    addPlagins

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,23 @@
 var gulp = require('gulp');
-var	concat = require('gulp-concat');
-var cssnano = require('gulp-cssnano');
-var clean = require('gulp-clean');
-var browserSync = require('browser-sync').create();
-var autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('default', ['clean'], function() {
-	gulp.run('develop');
-}); 
-gulp.task('develop', ['css', 'watch', 'browser-sync', 'assets']); 
+var rename = require ('gulp-rename');
+var postcss = require('gulp-postcss');
+var assets = require ('postcss-assets');
+var short = require('postcss-short');
 
 gulp.task('css', function () {
-	return gulp.src('styles/*.css')
-		.pipe(concat('styles.css'))
-		.pipe(cssnano())
-		.pipe(autoprefixer({
-			browser: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('build/'));
+    var processors = [
+		short,
+		assets ({
+			loadPaths: ['assets/img/'],
+			relativTo: '/styles/'
+		}),
+  ];
+    return gulp.src('./styles/styles.css')
+        .pipe(postcss(processors))
+		.pipe(rename('styleOut.css'))
+        .pipe(gulp.dest('./build/styles/'));
 });
 
-gulp.task('browser-sync', function() {
-	return browserSync.init({
-		server: {
-			baseDir: './build/'
-		}
-	});
-});
-
-gulp.task('watch', function() {
-	gulp.watch('*.css', ['css']);
-	gulp.watch('js/*.js', ['scripts']);
-});
-
-gulp.task('clean', function() {
-	return gulp.src('build/')
-		.pipe(clean());
+gulp.task ('default', function() {
+		gulp.watch('styles/*.css',['css'])
 })
-
-gulp.task('assets', function() {
-	return gulp.src('**/*.*')
-		.pipe(gulp.dest('build/'));
-});
