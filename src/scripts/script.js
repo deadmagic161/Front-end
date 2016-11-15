d81d682f3ea93691e4527e51da29c5e5cf677364
deadmagic161 <fernando093@yandex.ru>
2016-11-16 00:33:43 +0700

    add-script

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,20 @@
 window.onload = function () {
-	var show = document.getElementById('click');
-	var open = document.getElementById('show');
-	show.onclick= function () {
-		show.classList.toggle('_arrow-down-show');
-		open.classList.toggle('comment-text__opinion-new');
+	var show1 = document.getElementById('click1');
+	var show2 = document.getElementById('click2');
+	var show3 = document.getElementById('click3');
+	var open1 = document.getElementById('show1');
+	var open2 = document.getElementById('show2');
+	var open3 = document.getElementById('show3');
+	show1.onclick= function () {
+		show1.classList.toggle('_arrow-down-show');
+		open1.classList.toggle('comment-text__opinion-new');
+	};
+	show2.onclick= function () {
+		show2.classList.toggle('_arrow-down-show');
+		open2.classList.toggle('comment-text__opinion-new');
+	};
+	show3.onclick= function () {
+		show3.classList.toggle('_arrow-down-show');
+		open3.classList.toggle('comment-text__opinion-new');
 	};
 };
