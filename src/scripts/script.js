20da85c9d7bd871fa333474e1f37878c34697469
deadmagic161 <fernando093@yandex.ru>
2016-11-19 13:49:11 +0700

    fix script

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,20 +1,10 @@
 window.onload = function () {
-	var show1 = document.getElementById('click1');
-	var show2 = document.getElementById('click2');
-	var show3 = document.getElementById('click3');
-	var open1 = document.getElementById('show1');
-	var open2 = document.getElementById('show2');
-	var open3 = document.getElementById('show3');
-	show1.onclick= function () {
-		show1.classList.toggle('_arrow-down-show');
-		open1.classList.toggle('comment-text__opinion-new');
-	};
-	show2.onclick= function () {
-		show2.classList.toggle('_arrow-down-show');
-		open2.classList.toggle('comment-text__opinion-new');
-	};
-	show3.onclick= function () {
-		show3.classList.toggle('_arrow-down-show');
-		open3.classList.toggle('comment-text__opinion-new');
-	};
+	var buttons = document.querySelectorAll('.comment_button__arrow');
+	for (var comment_button__arrow  of buttons) {
+		comment_button__arrow.addEventListener('click', function(event) {
+			var parent = event.target.parentNode;
+			parent.classList.toggle('_open');
+			parent.classList.toggle('_show');
+		});
+	}
 };
