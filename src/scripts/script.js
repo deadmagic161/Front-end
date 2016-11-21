ae53e2daf5477521264803261ece33b46fdb4188
deadmagic161 <fernando093@yandex.ru>
2016-11-22 01:37:56 +0700

    I WIN

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,7 +2,7 @@ window.onload = function () {
 	var buttons = document.querySelectorAll('.comment_button__arrow');
 	for (var comment_button__arrow  of buttons) {
 		comment_button__arrow.addEventListener('click', function(event) {
-			var parent = event.target.parentNode;
+			var parent = event.target.closest('.comment-text');
 			parent.classList.toggle('_open');
 		});
 	}
