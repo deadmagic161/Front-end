d0375a76c34d6512a3a0f377d652a9555f253aa0
deadmagic161 <fernando093@yandex.ru>
2016-11-22 03:03:07 +0700

    finally fix

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,8 @@
 window.onload = function () {
 	var buttons = document.querySelectorAll('.comment_button__arrow');
-	for (var comment_button__arrow  of buttons) {
-		comment_button__arrow.addEventListener('click', function(event) {
+	
+	for (var button  of buttons) {
+		button.addEventListener('click', function(event) {
 			var parent = event.target.closest('.comment-text');
 			parent.classList.toggle('_open');
 		});
