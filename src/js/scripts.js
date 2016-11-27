1247fabdddc6256a144a011a9b997f865c3f7bbb
deadmagic161 <fernando093@yandex.ru>
2016-11-27 15:35:48 +0700

    del

--- src/js/scripts.js
+++
@@ -1,7 +0,0 @@
-var follow = document.querySelectorAll('.comment__sign');
-for (var comment__sign of follow) {
-	comment__sign.addEventListener('click', function(event) {
-		event.target.classList.toggle('_follow');
-	})
-}
-
