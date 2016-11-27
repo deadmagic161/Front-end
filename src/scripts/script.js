c01326648710d5ecc320562bf06f3c0bc8d6f1a6
deadmagic161 <fernando093@yandex.ru>
2016-11-27 19:02:05 +0700

    fix

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,3 @@
-window.onload = function () {
 	var buttons = document.querySelectorAll('.comment_button__arrow');
 	
 	for (var button  of buttons) {
@@ -8,6 +7,23 @@ window.onload = function () {
 		});
 	}
 
+	var more = document.getElementById("more-comments");
+
+	function load () {
+		var block1 = document.getElementById('block1');
+		var block2 = document.getElementById('block2');
+  		block1.classList.add('_unload');
+  		block2.classList.add('_unload');
+}
+
+	more.onclick = function() {
+		var load = document.querySelector('._unload');
+  		var show = document.querySelector('.comments');
+  		load.classList.remove('_unload');
+  		show.classList.add('_show');
+}
+
+
 	var follow = document.querySelectorAll('.comment__sign');
 	
 	for (var comment__sign of follow) {
@@ -16,5 +32,4 @@ window.onload = function () {
 	})
 }
 
-
-};
+window.onload = load();
