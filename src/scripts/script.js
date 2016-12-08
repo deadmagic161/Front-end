ee60fdcb471c09c7de3510ab9a1e1a5ef93b9ef3
AzizmuradovAR <Azizmuradovaziz@gmail.com>
2016-12-08 18:14:04 +0300

    fixed

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -35,8 +35,8 @@
 window.onload = load();
 
 var follow = document.querySelectorAll('.comment__sign');
-for (var comment__sign of follow) {
-	comment__sign.addEventListener('click', function(event) {
+for (var commentSign of follow) {
+	commentSign.addEventListener('click', function(event) {
 		event.target.classList.toggle('_follow');
 	})
 }
