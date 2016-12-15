2d059e695d5a6827871272cb7ba233ecba087a3b
Natalya Norboyeva <divonata@gmail.com>
2016-12-15 13:33:21 +0900

    here was 'git checkout develop src/assets'

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,31 +1,25 @@
-var trigger = document.querySelector('.book-preview__img_small');
+var triggers = document.querySelectorAll('.book-preview__img_small');
 
 var Gallery = (function() {
-    var images = document.querySelectorAll('.book-preview__img_small');
-
     var _init = function() {
-        _eventListeners();
+        _eventListener();
     };
 
-    var _eventListeners = function() {
+    var _eventListener = function() {
+        var images = document.querySelectorAll('.book-preview__img_small');
+
+        var eventHandler = function() {
+            console.log('Image clicked');
+        };
+
         for (var i = 0; i < images.length; i++ ) {
             images[i].addEventListener('click', eventHandler);
         }
     };
-
-    var eventHandler = function() {
-            var clickedImg = this;
-            var bigImg = document.querySelector('.book-preview__img_big');
-            var imagePathBuffer = clickedImg.style.backgroundImage;
-
-            clickedImg.style.backgroundImage = bigImg.style.backgroundImage;
-            bigImg.style.backgroundImage = imagePathBuffer;
-
-        };
 
     return {
         init: _init
     }
 })();
 
-trigger && Gallery.init();
+triggers && Gallery.init();
