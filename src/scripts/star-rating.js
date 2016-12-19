8ccccc65a648d5256ed31ece0c1a587bb7238bce
Azizmuradovar <azizmuradovaziz@gmail.com>
2016-12-19 14:54:14 +0300

    animated

--- src/scripts/star-rating.js
+++ src/scripts/star-rating.js
@@ -1,13 +1,15 @@
 (function(){
 	var stars = document.querySelectorAll('.rating');
+	var ratingValue = document.getElementsByClassName('rating-value');
 
 	for (var star of stars) {
 		star.addEventListener('click', function(event) {
 			for (var i = stars.length - 1; i >= 0; i--) {
-				stars[i].classList.remove('value');
+				stars[i].classList.remove('animated');
 			};
 
-			event.target.classList.add('value');
+			event.target.classList.add('animated');
+			ratingValue[0].innerHTML = this.getAttribute('data');
 
 			for (var i = stars.length - 1; i >= 0; i--) {
 				if (Number(stars[i].getAttribute('data')) <= Number(this.getAttribute('data'))) {
