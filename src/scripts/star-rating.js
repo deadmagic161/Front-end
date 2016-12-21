b606663c3831f971d2dc734c8b45f2ed6c097a36
Azizmuradovar <azizmuradovaziz@gmail.com>
2016-12-21 19:08:52 +0300

    module JS

--- src/scripts/star-rating.js
+++ src/scripts/star-rating.js
@@ -1,23 +1,27 @@
-(function(){
-	var stars = document.querySelectorAll('.rating');
-	var ratingValue = document.getElementsByClassName('rating-value');
+var stars = document.querySelectorAll('.rating');
+var ratingValue = document.getElementsByClassName('rating-value');
+var starModule = (function(){
+	var _init = function(){
+		for (var star of stars) {
+			star.addEventListener('click', function(event) {
+				for (var i = stars.length - 1; i >= 0; i--) {
+					stars[i].classList.remove('animated');
+					if (Number(stars[i].getAttribute('data')) <= Number(this.getAttribute('data'))) {
+						stars[i].classList.add('active');
+					}else {
+						stars[i].classList.remove('active');
+					}
+				};
 
-	for (var star of stars) {
-		star.addEventListener('click', function(event) {
-			for (var i = stars.length - 1; i >= 0; i--) {
-				stars[i].classList.remove('animated');
-			};
-
-			event.target.classList.add('animated');
-			ratingValue[0].innerHTML = this.getAttribute('data');
-
-			for (var i = stars.length - 1; i >= 0; i--) {
-				if (Number(stars[i].getAttribute('data')) <= Number(this.getAttribute('data'))) {
-					stars[i].classList.add('active');
-				}else {
-					stars[i].classList.remove('active');
-				}
-			}
-		})
+				event.target.classList.add('animated');
+				ratingValue[0].innerHTML = this.getAttribute('data');
+			});
+		};
 	}
-}());
+
+	return {
+		init: _init
+	}
+})();
+
+stars&&starModule.init();
