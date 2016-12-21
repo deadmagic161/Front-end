d3caed553350359b44f9ade2d4373c59940e19da
AzizmuradovAR <Azizmuradovaziz@gmail.com>
2016-12-21 21:02:05 +0300

    fixed

--- src/scripts/star-rating.js
+++ src/scripts/star-rating.js
@@ -1,27 +1,38 @@
 var stars = document.querySelectorAll('.rating');
-var ratingValue = document.getElementsByClassName('rating-value');
-var starModule = (function(){
-	var _init = function(){
-		for (var star of stars) {
-			star.addEventListener('click', function(event) {
-				for (var i = stars.length - 1; i >= 0; i--) {
-					stars[i].classList.remove('animated');
-					if (Number(stars[i].getAttribute('data')) <= Number(this.getAttribute('data'))) {
-						stars[i].classList.add('active');
-					}else {
-						stars[i].classList.remove('active');
-					}
-				};
 
-				event.target.classList.add('animated');
-				ratingValue[0].innerHTML = this.getAttribute('data');
-			});
+var starModule = (function() {
+	var _init = function() {
+		_eventListeners();
+	};
+
+	var _eventListeners = function() {
+		for (var star of stars) {
+			star.addEventListener('click', _setCurrentStar);
 		};
-	}
+	};
+
+	var _setCurrentStar = function(event) {
+		var elem = event.target;
+		var ratingValue = document.getElementsByClassName('rating-value');
+
+		var eventStar = Number(elem.getAttribute('data'));
+
+		for (var i = stars.length - 1; i >= 0; i--) {
+			var currentStar = Number(stars[i].getAttribute('data'));
+				
+			stars[i].classList.remove('animated');
+
+			currentStar <= eventStar ? stars[i].classList.add('active')
+				: stars[i].classList.remove('active');
+		};
+
+		elem.classList.add('animated');
+		ratingValue[0].innerHTML = elem.getAttribute('data');
+	};
 
 	return {
 		init: _init
-	}
+	};
 })();
 
-stars&&starModule.init();
+stars && starModule.init();
