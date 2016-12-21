e3e4e06660c41049108d61f6fe3499f87019c1d3
Natalie Norb <divonata@gmail.com>
2016-12-22 00:45:40 +0900

    правки стиля кода

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -27,11 +27,12 @@
 
 	var follow = document.querySelectorAll('.comment__sign');
 	
-	for (var comment__sign of follow) {
-	comment__sign.addEventListener('click', function(event) {
+	for (var commentSign of follow) {
+	    comment__sign.addEventListener('click', function(event) {
 		event.target.classList.toggle('_follow');
 	})
 }
+
 window.onload = load();
 
 var follow = document.querySelectorAll('.comment__sign');
